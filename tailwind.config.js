c9f068c9ce29ea8ff1eac7b54620142c993b1b63
EagleEye <agentextern@gmail.com>
2026-06-21 21:18:00 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "rgba(255,255,255,0.06)",
+        ring: "rgba(57,255,107,0.5)",
+        foreground: "#e0f2fe",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
